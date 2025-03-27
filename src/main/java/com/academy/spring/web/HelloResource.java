88916597c21652b6aba162c87ae54ed03e72749b
Axel Reinno Fabiyanto <axel.fabiyanto@nostratech.com>
2025-03-27 18:14:43 +0700

    feat: add multiple profiles and use application properties

--- src/main/java/com/academy/spring/web/HelloResource.java
+++ src/main/java/com/academy/spring/web/HelloResource.java
@@ -1,19 +1,30 @@
 package com.academy.spring.web;
 
+import com.academy.spring.configs.AppConfig;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/v1/hello")
 public class HelloResource {
 
+    private AppConfig properties;
 
     @RequestMapping(method = RequestMethod.GET)
     public String hello() {
         return "hello";
     }
 
+    @RequestMapping(method = RequestMethod.GET, value = "/print")
+    public String print() {
+        return "name: " + properties.getName() + ", email: " + properties.getEmail();
+    }
 
-
+    @Autowired
+    public void setAppConfig(AppConfig properties) {
+        this.properties = properties;
+    }
 }
