cabe7cd4904c2548d07f7987e672bc971553cf24
Axel Reinno Fabiyanto <axel.fabiyanto@nostratech.com>
2025-03-27 14:06:22 +0700

    feat: initiate project

---
+++ src/main/java/com/academy/spring/web/HelloResource.java
@@ -0,0 +1,19 @@
+package com.academy.spring.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/v1/hello")
+public class HelloResource {
+
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String hello() {
+        return "hello";
+    }
+
+
+
+}
